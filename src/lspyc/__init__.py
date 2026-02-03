9be0e6f1d7e20b6e475fcb4ba40c32fbdca1cfad
zhanpengliu <zhanpengliu@tencent.com>
2026-02-03 14:42:26 +0800

    Chore: export symbols

--- src/lspyc/__init__.py
+++ src/lspyc/__init__.py
@@ -1,7 +1,29 @@
 """LSP Python Client - A library for LSP server management and usage."""
 
-from .handle import LspStdioHandle
+from .handle import (
+    DockerHandleFactory,
+    HandleFactory,
+    LspHandle,
+    LspStdioHandle,
+    LspWsHandle,
+    NativeHandleFactory,
+    WebSocketHandleFactory,
+)
+from .handle.protocol import DocumentSymbol, Location
+from .mlclient import MutilLangClient
 from .settings import LspycSettings
 
 __version__ = "0.1.0"
-__all__ = ["LspStdioHandle", "LspycSettings"]
+__all__ = [
+    "DocumentSymbol",
+    "Location",
+    "HandleFactory",
+    "DockerHandleFactory",
+    "NativeHandleFactory",
+    "WebSocketHandleFactory",
+    "LspHandle",
+    "LspStdioHandle",
+    "LspWsHandle",
+    "LspycSettings",
+    "MutilLangClient",
+]
